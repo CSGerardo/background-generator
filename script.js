72b1e812a14d00c75e0b35992a3a9133791f3336
CSGerardo <t.carlos.cg@gmail.com>
2024-03-30 15:21:59 -0700

    changed color values

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-var css = document.querySelector("h3");
-var color1 = document.querySelector(".color1");
-var color2 = document.querySelector(".color2");
-var body = document.getElementById("gradient");
+const css = document.querySelector("h3");
+const color1 = document.querySelector(".color1");
+const color2 = document.querySelector(".color2");
+const body = document.getElementById("gradient");
+
+body.style.background = 
+"linear-gradient(to right, " 
++ color1.value 
++ ", " 
++ color2.value 
++ ")";
+
+css.textContent = body.style.background + ";";
 
 function setGradient() {
 	body.style.background = 
